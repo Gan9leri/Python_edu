48ff7fa0ba1e52c1ba33c429affc88dd29596c9b
Овсянников Александр Александрович <master980@yandex.ru>
2023-10-11 22:31:39 +0300

    Create task2_2_3.py

---
+++ Программирование на Python/31-40/35. task2_2_3/task2_2_3.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 11 22:28:59 2023
+
+@author: maste
+"""
+
+while True:
+    a = int(input())
+    if a < 10 : continue
+    elif a > 100 : break
+    else: print(a)
