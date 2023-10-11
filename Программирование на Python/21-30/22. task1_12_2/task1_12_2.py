1e5004e9efc83b150c97ff150febda85ecf773d8
Овсянников Александр Александрович <master980@yandex.ru>
2023-10-11 21:23:40 +0300

    Create task1_12_2.py

---
+++ Программирование на Python/21-30/22. task1_12_2/task1_12_2.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import math
+num = int(input())
+print ( ( (num > -15) and (num <= 12) ) or ( (num > 14) and (num < 17) ) or ( (num >= 19) and (num < math.inf)) )
