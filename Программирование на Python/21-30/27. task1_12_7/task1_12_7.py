edcbb5f363b4c4019491b06dd2b52381c5482993
Овсянников Александр Александрович <master980@yandex.ru>
2023-10-11 21:56:27 +0300

    Create task1_12_7.py

---
+++ Программирование на Python/21-30/27. task1_12_7/task1_12_7.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 11 21:23:34 2023
+
+@author: maste
+"""
+a, b, c, d, e, f = input()
+a, b, c, d, e, f = int(a), int(b), int(c), int(d), int(e), int(f)
+if a + b + c == d + e + f : print('Счастливый')
+else : print('Обычный')
