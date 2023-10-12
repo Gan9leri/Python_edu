0d300dc9355a9bf8be24ed1d49bd44f9991e5b8a
Овсянников Александр Александрович <master980@yandex.ru>
2023-10-12 13:43:56 +0300

    Create task2_4_2.py

---
+++ Программирование на Python/31-40/39. task2_4_2/task2_4_2.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 12 13:40:59 2023
+
+@author: maste
+"""
+
+s = 'abcdefghijk'
+print(s[3:6], s[:6], s[3:], s[::-1], s[-3:], s[:-6], s[-1:-10:-2])
+
+
