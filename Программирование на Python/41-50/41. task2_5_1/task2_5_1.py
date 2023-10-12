82f7331720983627ebb80e95830fb17b1d760b19
Овсянников Александр Александрович <master980@yandex.ru>
2023-10-12 14:11:08 +0300

    Create task2_5_1.py

---
+++ Программирование на Python/41-50/41. task2_5_1/task2_5_1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 12 13:44:31 2023
+
+@author: maste
+"""
+students = ['Ivan', 'Masha', 'Sasha']
+students += ['Olga']
+students += 'Olga'
+
+print(students)
+print(len(students))
